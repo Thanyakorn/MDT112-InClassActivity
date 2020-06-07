6affd29b59a47d4233e77d322bab0b58049d370b
thanyakorn.nak <thanyakorn.nak@mail.kmutt.ac.th>
2020-06-07 15:00:50 +0700

    02Update

--- project/project.ino
+++ project/project.ino
@@ -5,26 +5,66 @@
 
 Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 
+const int analogInPin = A0; //ประกาศขา
+const int relay = 13;  //ขา Relay
 
-void setup()
-{
-    Serial.begin(9600);
+int Moisture = 0;        // ตัวแปรค่า Analog
+int outputValue = 0;        // ตัวแปรสำหรับ Map เพื่อคิด %
 
+void setup(){
 
-}
-void loop()
-{
-    int sensorValue = analogRead(A0); 
+  
+  Serial.begin(9600);
 
-if (sensorValue >=600 && sensorValue <=800){
-Serial.println(sensorValue);
-    
+  pinMode(relay, OUTPUT);
+  oled.begin(0x3C); // Address 0x3C for 128x32
+  oled.clearDisplay();
+
+  delay(1000);
+
   }
+
+void loop(){
+  
+  Moisture = analogRead(analogInPin);
+  outputValue = map(Moisture, 0, 1023, 100, 0);
+
+  oled.clearDisplay();
+  oled.setCursor(70, 0);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setTextSize(1);
+  oled.print(outputValue);
+
+  oled.setCursor(0, 0);
+  oled.setTextSize(1);
+  oled.setTextColor(BLACK, WHITE);
+  oled.print("Moisture =");
+
+  oled.setCursor(100, 0);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setTextSize(1);
+  oled.print(" %");
+
+  oled.setCursor(100, 20);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setTextSize(1);
+  oled.print(" C");
+
+  oled.setCursor(0, 20);
+  oled.setTextSize(1);
+  oled.setTextColor(BLACK, WHITE);
+  oled.println("temperature =");
+
+
+  if (outputValue <= 40) {  //ตั้งค่า % ที่ต้องการจะรดน้ำต้นไม้
+    digitalWrite(relay, HIGH);
+  }
+
   else {
-Serial.println(sensorValue);
-    
+    digitalWrite(relay, LOW); //จะหยุดเมื่อน้อยกว่า 40
   }
-delay(1000);
 
-
+  oled.display();
+  delay(1000);
+  
 }
